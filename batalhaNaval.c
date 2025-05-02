9f16dd980d0b5896e8d4a1724ef71f9044c58a85
vivissW <vitoriawinner1182@gmail.com>
2025-05-02 20:13:23 -0300

    Novato BatalhaNaval.c

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,10 +1,9 @@
-#include <stdio.h>
+
 
 // Desafio Batalha Naval - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 
-int main() {
     // Nível Novato - Posicionamento dos Navios
    #include <stdio.h>
 
